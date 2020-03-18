43a7327e14e92609099e644f9a8b90639cd47c91
anthony mcmullen <amcmullen9291@gmail.com>
2020-03-18 16:30:17 +0000

    Automatically backed up by Learn

--- lib/transfer.rb
+++ lib/transfer.rb
@@ -18,7 +18,7 @@ class Transfer
   def execute_transaction
     if @sender.balanace< @amount || !@sender.valid? 
       @status = "rejected"
-      
+      "Transaction rejected. Please check your account balance."
     end
   end 
 
